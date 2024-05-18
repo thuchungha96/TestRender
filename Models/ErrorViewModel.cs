e7211269467f7fe293bea25c7b8cfc46dfeab57a
ha.thuchung96@outlook.com <ha.thuchung96@outlook.com>
2024-05-18 10:17:40 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestRender.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
